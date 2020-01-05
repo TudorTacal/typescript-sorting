d3a1c89f2ba322cde4a39e599435a876ed88bd94
TudorTacal <tudortacal@gmail.com>
2020-01-05 11:50:16 +0000

    setup project - create Sorter class

---
+++ build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var _a;
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    _a = [this.collection[j + 1], this.collection[j]], this.collection[j] = _a[0], this.collection[j + 1] = _a[1];
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
