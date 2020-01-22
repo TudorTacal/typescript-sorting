bb29e4d8a3cb78d55ea82d7cd6b1ffc8fe5a59b5
TudorTacal <tudortacal@gmail.com>
2020-01-22 09:29:47 +0000

    create linked list collection and refactor to abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const collection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(collection);
 // sorter.sort();
 // console.log(sorter.collection);
 
-const charactersCollection = new CharactersCollection('dcba');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('dcba');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(1);
+linkedList.add(10);
+linkedList.add(-27);
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection);
+linkedList.print();
